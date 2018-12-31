12a034369324a0d9ee19cfa378cc7abb4ffae1ee
Liam Hinzman <liam.hinzman@gmail.com>
2018-12-31 15:14:28 -0700

    Fixes to validation and accuracy metrics

--- intro-to-pytorch/Fashion-MNIST.py
+++ intro-to-pytorch/Fashion-MNIST.py
@@ -37,13 +37,13 @@ epochs = 10
 model = nn.Sequential(
             nn.Linear(input_dimension, hidden_layer_1_dimension),
             nn.ReLU(),
-            nn.Dropout(dropout_rate),
+            # nn.Dropout(dropout_rate),
             nn.Linear(hidden_layer_1_dimension, hidden_layer_2_dimension),
             nn.ReLU(),
-            nn.Dropout(dropout_rate),
+            # nn.Dropout(dropout_rate),
             nn.Linear(hidden_layer_2_dimension, hidden_layer_3_dimension),
             nn.ReLU(),
-            nn.Dropout(dropout_rate),
+            # nn.Dropout(dropout_rate),
             nn.Linear(hidden_layer_3_dimension, output_dimension),
             nn.LogSoftmax(dim=1)
         ).to(device)
@@ -52,13 +52,12 @@ print(model)
 
 # Loss and optimizer
 criterion = nn.NLLLoss()
-learning_rate = 1e-4
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.003)
 
 training_loss = []
 validation_loss = []
 # Training loop
-for t in range(epochs):
+for e in range(epochs):
     running_loss = 0
     for images, labels in trainloader:
         images = images.view(images.shape[0], -1).cuda()
@@ -72,39 +71,62 @@ for t in range(epochs):
         optimizer.step()
 
         running_loss += loss.item()
-    print(f"Training loss: {running_loss/len(trainloader)}")
-    training_loss.append(running_loss/len(trainloader))
 
     # Evaluate model on validation set
     # Turn off gradients
-    accuracy = 0
-    with torch.no_grad():
-        # Set model to evaluation mode
-        # Turn off dropout for inference
-        model.eval()
+    else:
+        test_loss = 0
+        accuracy = 0
+        with torch.no_grad():
+            # Set model to evaluation mode
+            # Turn off dropout for inference
+            model.eval()
 
-        for images, labels in testloader:
-            images = images.view(images.shape[0], -1).cuda()
-            ps = torch.exp(model(images))
+            for images, labels in testloader:
+                images = images.view(images.shape[0], -1).cuda()
+                labels = labels.cuda()
 
-            # Get the top-1 prediction and class
-            top_p, top_class = ps.topk(1, dim=1)
+                log_ps = model(images).to(device)
+                test_loss += criterion(log_ps, labels)
 
-            # top_class is tensor with shape (64, 1)
-            # labels is 1D with shape (64)
-            # labels has to be reshaped to have equality work
-            correct_prediction = top_class == labels.view(*top_class.shape).cuda()
+                ps = torch.exp(log_ps)
+                # Get the top-1 prediction and class
+                top_p, top_class = ps.topk(1, dim=1)
 
-            # correct_prediction is a ByteTensor and has to be converted to a FloatTensor for torch.mean
-            accuracy += torch.mean(correct_prediction.type(torch.FloatTensor))
-        print(f"Accuracy: {accuracy/len(testloader)}")
-        validation_loss.append(accuracy/len(testloader))
+                # top_class is tensor with shape (64, 1)
+                # labels is 1D with shape (64)
+                # labels has to be reshaped to have equality work
+                num_correct_predictions = top_class == labels.view(*top_class.shape).cuda()
 
-    # Set model back to train mode
-    model.train()
+                # correct_prediction is a ByteTensor and has to be converted to a FloatTensor for torch.mean
+                accuracy += torch.mean(num_correct_predictions.type(torch.FloatTensor))
+
+        training_loss.append(running_loss/len(trainloader))
+        validation_loss.append(test_loss/len(testloader))
+
+        print("Epoch: {}/{}.. ".format(e+1, epochs),
+                  "Training Loss: {:.3f}.. ".format(running_loss/len(trainloader)),
+                  "Test Loss: {:.3f}.. ".format(test_loss/len(testloader)),
+                  "Test Accuracy: {:.3f}".format(accuracy/len(testloader)))
+
+        # Set model back to train mode
+        model.train()
+
+
+# Plot training and validation loss over epochs
+plt.plot(training_loss, label="Training loss")
+plt.plot(validation_loss, label="Test loss")
+plt.legend(frameon=False)
+
+# Axis labels
+plt.ylabel("Loss")
+plt.xlabel("Epoch")
+plt.show()
 
 
 # Test out network
+model.eval()
+
 dataiter = iter(testloader)
 images, labels = dataiter.next()
 img = images[0]
@@ -112,29 +134,12 @@ img = images[0]
 # Convert 2D image to 1D vector
 img = img.resize_(1, 784).cuda()
 
-# Class probabilities (softmax) for img
-ps = torch.exp(model(img)).cpu()
+with torch.no_grad():
+    output = model.forward(img)
 
+# Class probabilities (softmax) for img
+ps = torch.exp(output).cpu()
 
 # Plot the image and probabilities
 helper.view_classify(img.resize_(1, 28, 28).cpu(), ps, version='Fashion')
-plt.show()
-
-
-# Plot training and validation loss over epochs
-plt.plot(training_loss, 'r')   # Plot in red
-plt.plot(validation_loss, 'b') # Plot in blue
-
-# Legend
-red_patch = mlines.Line2D([], [], color='red', label='Training Loss')
-blue_patch = mlines.Line2D([], [], color='blue', label='Validation Loss')
-plt.legend(handles=[red_patch, blue_patch])
-
-# Only show integer values for x axis
-xint = range(0, len(training_loss))
-plt.xticks(xint)
-
-# Axis labels
-plt.ylabel("Loss")
-plt.xlabel("Epoch")
 plt.show()
