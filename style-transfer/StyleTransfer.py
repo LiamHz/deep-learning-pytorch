cd64cef28c3414bcc7ff5d87be4fc9baf21d70bc
Liam Hinzman <liam.hinzman@gmail.com>
2019-01-07 23:04:23 -0700

    Style transfer update

--- style-transfer/StyleTransfer.py
+++ style-transfer/StyleTransfer.py
@@ -1,15 +1,19 @@
-from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
 
 import torch
 import torch.optim as optim
-from torchvision import transforms, models
+from torchvision import models
 
-# get the "features" portion of VGG19 ("classifier" portion not needed)
+from helper import *
+
+# Get the "features" portion of VGG19
+# The classifier portion is not needed for style transfer
 vgg = models.vgg19(pretrained=True).features
 
 # Freeze all VGG parameters
+# Since only activations are measured for each conv layer
+# Network weights aren't modified
 for param in vgg.parameters():
     param.requires_grad_(False)
 
@@ -19,48 +23,6 @@ vgg.to(device)
 
 print(vgg)
 
-# HELPER FUNCTION
-# FROM UDACITY
-def load_image(img_path, max_size=400, shape=None):
-    ''' Load in and transform an image, making sure the image
-       is <= 400 pixels in the x-y dims.'''
-
-    image = Image.open(img_path).convert('RGB')
-
-    # large images will slow down processing
-    if max(image.size) > max_size:
-        size = max_size
-    else:
-        size = max(image.size)
-
-    if shape is not None:
-        size = shape
-
-    in_transform = transforms.Compose([
-                        transforms.Resize(size),
-                        transforms.ToTensor(),
-                        transforms.Normalize((0.485, 0.456, 0.406),
-                                             (0.229, 0.224, 0.225))])
-
-    # discard the transparent, alpha channel (that's the :3) and add the batch dimension
-    image = in_transform(image)[:3,:,:].unsqueeze(0)
-
-    return image
-
-# helper function for un-normalizing an image
-# and converting it from a Tensor image to a NumPy image for display
-def im_convert(tensor):
-    """ Display a tensor as an image. """
-
-    image = tensor.to("cpu").clone().detach()
-    image = image.numpy().squeeze()
-    image = image.transpose(1,2,0)
-    image = image * np.array((0.229, 0.224, 0.225)) + np.array((0.485, 0.456, 0.406))
-    image = image.clip(0, 1)
-
-    return image
-
-
 # Load in content and style image
 content = load_image('images/gandalf.jpg').to(device)
 # Resize style to match content, makes code easier
@@ -68,18 +30,18 @@ style = load_image('images/tokidoki.jpg', shape=content.shape[-2:]).to(device)
 
 # display the images
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
-# content and style ims side-by-side
 ax1.imshow(im_convert(content))
 ax2.imshow(im_convert(style))
 plt.show()
 
 def get_features(image, model, layers=None):
-    """ Run an image forward through a model and get the features for
-        a set of layers. Default layers are for VGGNet matching Gatys et al (2016)
+    """
+    Run an image forward through a model and get the features for a set of layers.
+    Default layers are for VGGNet matching Gatys et al (2016)
     """
 
-    ## TODO: Complete mapping layer names of PyTorch's VGGNet to names from the paper
-    ## Need the layers for the content and style representations of an image
+    # Dictionary of layers needed from VGG for style transfer
+    # Style representation is first conv layer from each block
     if layers is None:
         layers = {'0': 'conv1_1',
                   '5': 'conv2_1',
@@ -99,69 +61,69 @@ def get_features(image, model, layers=None):
     return features
 
 def gram_matrix(tensor):
-    """ Calculate the Gram Matrix of a given tensor
-        Gram Matrix: https://en.wikipedia.org/wiki/Gramian_matrix
+    """
+    Calculate the Gram Matrix of a given tensor
     """
 
-    # get the batch_size, depth, height, and width of the Tensor
+    # Get the batch_size, depth, height, and width of the Tensor
     # reshape it, so we're multiplying the features for each channel
     _, d, h, w = tensor.size()
     tensor = tensor.view(d, h*w)
 
-    # calculate the gram matrix
+    # Calculate the gram matrix
     gram = torch.mm(tensor, tensor.t())
 
     return gram
 
-# UDACITY
-# get content and style features only once before forming the target image
+# Get content and style features only once before forming the target image
 content_features = get_features(content, vgg)
 style_features = get_features(style, vgg)
 
-# calculate the gram matrices for each layer of our style representation
+# Calculate the gram matrices for each layer of our style representation
 style_grams = {layer: gram_matrix(style_features[layer]) for layer in style_features}
 
-# create a third "target" image and prep it for change
-# it is a good idea to start of with the target as a copy of our *content* image
-# then iteratively change its style
+# Create a third target image (copy of content iamge)
+# This is the image that will have its style iteratively changed
 target = content.clone().requires_grad_(True).to(device)
 
-# weights for each style layer
-# weighting earlier layers more will result in *larger* style artifacts
-# notice we are excluding `conv4_2` our content representation
+# Weights for each style layer
+# Weighting earlier layers more will result in larger style artifacts
+# conv4_2 is excluded because it is the content representation
 style_weights = {'conv1_1': 1.,
                  'conv2_1': 0.8,
                  'conv3_1': 0.5,
                  'conv4_1': 0.3,
                  'conv5_1': 0.1}
 
-# you may choose to leave these as is
-content_weight = 1  # alpha
-style_weight = 1e6  # beta
+# A higher style weight may make content unrecognizable
+content_weight = 1  # alpha in paper
+style_weight = 1e6  # beta in paper
 
-
-
-# for displaying the target image, intermittently
+# Show target image every x steps
 show_every = 400
 
-# iteration hyperparameters
+# Optimizer hyperparameters
 optimizer = optim.Adam([target], lr=0.003)
-steps = 2000  # decide how many iterations to update your image (5000)
 
+# How many iterations to update content image
+steps = 2000
+
+print("Beginning training")
 for ii in range(1, steps+1):
-    if ii % 50 == 0:
+    if ii % 100 == 0:
         print("Step #", ii, '/', steps+1)
-        
+
     # Calculate the content loss
     target_features = get_features(target, vgg)
     content_loss = torch.mean((target_features['conv4_2'] - content_features['conv4_2'])**2)
 
-    # the style loss
-    # initialize the style loss to 0
+    # Initialize style loss to 0
+    # The world was perfect before the model started training
     style_loss = 0
-    # iterate through each style layer and add to the style loss
+
+    # Iterate through each style layer and add to the style loss
     for layer in style_weights:
-        # get the "target" style representation for the layer
+        # Get the target (goal) style representation for the layer
         target_feature = target_features[layer]
         _, d, h, w = target_feature.shape
 
@@ -171,7 +133,7 @@ for ii in range(1, steps+1):
         # Get the style representation
         style_gram = style_grams[layer]
 
-        # Calculate the style loss for one layer, weighted appropriately
+        # Calculate the weighted style loss for one layer
         layer_style_loss = style_weights[layer] * torch.mean((target_gram - style_gram)**2)
 
         # add to the style loss
@@ -186,14 +148,14 @@ for ii in range(1, steps+1):
     total_loss.backward()
     optimizer.step()
 
-    # display intermediate images and print the loss
+    # Display intermediate images and print the loss
     if  ii % show_every == 0:
         print('Total loss: ', total_loss.item())
         plt.imshow(im_convert(target))
         plt.show()
 
 
-# display content and final, target image
+# Display content and final, target image
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
 ax1.imshow(im_convert(content))
 ax2.imshow(im_convert(target))
---
+++ style-transfer/helper.py
@@ -0,0 +1,44 @@
+from PIL import Image
+import numpy as np
+
+import torch
+from torchvision import transforms
+
+def load_image(img_path, max_size=400, shape=None):
+    ''' Load in and transform an image, making sure the image
+       is <= 400 pixels in the x-y dims.'''
+
+    image = Image.open(img_path).convert('RGB')
+
+    # large images will slow down processing
+    if max(image.size) > max_size:
+        size = max_size
+    else:
+        size = max(image.size)
+
+    if shape is not None:
+        size = shape
+
+    in_transform = transforms.Compose([
+                        transforms.Resize(size),
+                        transforms.ToTensor(),
+                        transforms.Normalize((0.485, 0.456, 0.406),
+                                             (0.229, 0.224, 0.225))])
+
+    # discard the transparent, alpha channel (that's the :3) and add the batch dimension
+    image = in_transform(image)[:3,:,:].unsqueeze(0)
+
+    return image
+
+# helper function for un-normalizing an image
+# and converting it from a Tensor image to a NumPy image for display
+def im_convert(tensor):
+    """ Display a tensor as an image. """
+
+    image = tensor.to("cpu").clone().detach()
+    image = image.numpy().squeeze()
+    image = image.transpose(1,2,0)
+    image = image * np.array((0.229, 0.224, 0.225)) + np.array((0.485, 0.456, 0.406))
+    image = image.clip(0, 1)
+
+    return image
